788d2406be850ed67df8c7388b51fee934d9da71
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-05-02 19:16:56 -0300

    a

---
+++ cockatiel-adventures/Scenes/buttons.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var area = $Area2D
+@onready var sprite = $Area2D/Sprite2D
+@onready var animation_player = $AnimationPlayer
+
+func _ready():
+	area.body_entered.connect(_on_body_entered)
+	area.body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body):
+	if body.name.begins_with("Player"):
+		animation_player.play("press")
+
+func _on_body_exited(body):
+	if body.name.begins_with("Player"):
+		animation_player.play("release")
