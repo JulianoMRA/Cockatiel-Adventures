9015d23fec9bdc2bd897b4d33fa9a4431aa6c9fa
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-05-04 18:27:22 -0300

    Configurando plataforma e botão.

--- cockatiel-adventures/Scenes/area_2d.gd
+++ cockatiel-adventures/Scenes/area_2d.gd
@@ -1,17 +1,51 @@
 extends Area2D
 
 @onready var anim = $AnimationPlayer
-@onready var sprite = $Sprite2D
+@export var target_platform: NodePath
+
+var _is_pressed = false
 
 func _ready():
+	# Garante que a animação começa solta
+	anim.play("Release")
+	
+	# Garante que a plataforma começa ativa
+	var platform = get_node(target_platform)
+	platform.get_node("AnimatedSprite2D").visible = false
+	platform.get_node("Sprite2D").visible = true
+	platform.get_node("CollisionShape2D").disabled = false
+
+	# Aguarda um frame antes de ativar o monitoring, evitando detectar o player no início
+	await get_tree().process_frame
 	monitoring = true
+
+	# Conecta os sinais
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body):
-	if body.name.begins_with("Player"):
-		anim.play("Press")
+	if _is_pressed:
+		return
+
+	_is_pressed = true
+	anim.play("Press")
+	var platform = get_node(target_platform)
+
+	platform.get_node("Sprite2D").visible = false
+	platform.get_node("AnimatedSprite2D").visible = true
+	platform.get_node("AnimatedSprite2D").play("Desativando")
+
+	await platform.get_node("AnimatedSprite2D").animation_finished
+	platform.get_node("CollisionShape2D").disabled = true
 
 func _on_body_exited(body):
-	if body.name.begins_with("Player"):
-		anim.play("Release")
+	_is_pressed = false
+	anim.play("Release")
+	var platform = get_node(target_platform)
+
+	platform.get_node("AnimatedSprite2D").play("Ativando")
+
+	await platform.get_node("AnimatedSprite2D").animation_finished
+	platform.get_node("AnimatedSprite2D").visible = false
+	platform.get_node("Sprite2D").visible = true
+	platform.get_node("CollisionShape2D").disabled = false
